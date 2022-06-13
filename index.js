926abc584340e965e5731ad81f6b45536d708036
Francisco Quijada <fquijada@creditu.com>
2022-06-12 20:33:13 -0400

    first test tesseract

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const multer = require('multer')
+
+const tesseract = require("node-tesseract-ocr");
+
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname + '/uploads')))
+
+app.set('view engine', "ejs")
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        );
+    },
+});
+
+const upload = multer({storage:storage})
+
+app.get('/', (req, res) => {
+    res.render('index',{data:''})
+})
+
+app.post('/extracttextfromimage', upload.single('file'), (req, res) => {
+    console.log(req.file.path)
+
+    const config = {
+        lang: "eng",
+        oem: 1,
+        psm: 3,
+    };
+
+    tesseract
+        .recognize(req.file.path, config)
+        .then((text) => {
+            console.log("Result:", text);
+
+            res.render('index',{data:text})
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+})
+
+
+app.listen(5000, () => {
+    console.log("port 5000")
+})
